f377d16e5d3670b1666b3faaf054e193491133d3
Dark <prasathsiva2004@gmail.com>
2024-03-22 07:20:32 +0530

    login

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react'
 import {Route,Routes} from 'react-router-dom'
 import Home from './Home'
 import Login from './Login'
+import Cookies from 'universal-cookie'
+const cookies=new Cookies()
 const App = () => {
+  console.log(cookies.get('cookies'))
   return (
     // <Home />
     <Login />
--- src/Login.js
+++ src/Login.js
@@ -10,9 +10,14 @@ const Login = () => {
     }
   return (
     <main>
+      <div id='ball_1'></div>
         <div id='login'>
+             <div id='input'>
             <input type='text' onChange={(e)=>setValues(pre=>({...pre,name:e.target.value}))} placeholder='name' />
+            </div>
+            <div id='input'>
             <input type='text' onChange={(e)=>setValues(pre=>({...pre,pass:e.target.value}))} placeholder='password'/>
+            </div>
             <div id='Submit_form'>
             <button onClick={()=>handleSubmit()}>Login</button>
             </div>
